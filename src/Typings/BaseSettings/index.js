3b1757b2d367af4663bba00395a3556c7d9081dd
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 06:28:24 -0600

    Update index.js

--- src/Typings/BaseSettings/index.js
+++ src/Typings/BaseSettings/index.js
@@ -3,13 +3,17 @@ const aepl = require('aepl');
 const OUT = new Soup(Object);
 
 class BaseSettings {
-    constructor(settings) {
-        this.objectHideFlags = this.__getSetting("m_ObjectHideFlags", settings);
-        this.serializedVersion = this.__getSetting("serializedVersion", settings);
+    constructor(settings=null) {
+        if (settings) {
+            this.objectHideFlags = this.__getSetting("m_ObjectHideFlags", settings);
+            this.serializedVersion = this.__getSetting("serializedVersion", settings);
+        }
     }
 
     __getSetting(setting, settings, settingSettings={}) {
-        let raw = settings.filter( s => s.includes(setting) )[0].replace(`${setting}:`, "").trim();
+        let raw = settings.filter( s => s.includes(setting))[0].replace(`${setting}:`, "").trim();
+        if (raw == "") raw = "undefined";
+
         let stuff = (settingSettings.keepAsString) ? raw : (settingSettings.type) ? eval(`new ${type.name}(${raw})`) : eval(raw)
         return (settingSettings.callback) ? settingSettings.callback(stuff) : stuff ;
     }
