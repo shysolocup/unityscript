8a6995b7172ab52af30bef856f631b1ae103c545
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 08:39:18 -0600

    Update index.js

--- src/Typings/RenderSettings/index.js
+++ src/Typings/RenderSettings/index.js
@@ -7,6 +7,10 @@ class RenderSettings {
         let baseSettings = this.extend(this.parent.BaseSettings, settings);
 
         this.fog = new this.parent.Fog(settings, baseSettings);
+        this.ambient = new this.parent.Ambient(settings, baseSettings);
+        this.subtractiveShadowColor = this.__getSetting("m_SubtractiveShadowColor", settings);
+        this.skybox = new this.parent.Skybox(settings, baseSettings);
+        this.halo = new this.parent.Halo(settings, baseSettings);
     }
 }
 
