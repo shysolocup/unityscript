814f8a9655da42ac9719a77c1775436da85df5be
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 06:57:03 -0600

    Update index.js

--- src/Typings/Scene/index.js
+++ src/Typings/Scene/index.js
@@ -3,7 +3,26 @@ const aepl = require('aepl');
 const OUT = new Soup(Object);
 
 
-class Scene {}
+class Scene {
+    constructor() {
+
+    }
+
+    translate(data, meta) {
+        let stuff = data.split("--- !u");
+        stuff.shift();
+
+        stuff = stuff.map( s => {
+            s = s.split(/(\r\n|\n|\r)/gm);
+            s.shift();
+            s = s.filter( thing => !(thing == "\r\n") );
+            return s;
+        });
+
+        let occlusion = stuff.filter( s => s[0].includes("OcclusionCullingSettings"))[0];
+        this.occlusionCullingSettings = new this.parent.OcclusionCullingSettings(occlusion);
+    }
+}
 
 
 OUT.push("Scene", aepl.init("Scene", Scene));
