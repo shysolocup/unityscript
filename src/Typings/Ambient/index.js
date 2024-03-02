f5b55148614357df86b20716d60fad6c5bc854aa
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 07:46:14 -0600

    Update index.js

--- src/Typings/Ambient/index.js
+++ src/Typings/Ambient/index.js
@@ -8,10 +8,12 @@ class Ambient {
     constructor(settings, baseSettings) {
         Object.assign(this, baseSettings);
         util.inherits(this.constructor, baseSettings.constructor);
+
+        let Color = this.parent.Color;
         
-        this.skyColor = this.__getSetting("m_AmbientSkyColor", settings);
-        this.equatorColor = this.__getSetting("m_AmbientEquatorColor", settings);
-        this.groundColor = this.__getSetting("m_AmbientGroundColor", settings);
+        this.skyColor = Color.fromRGBA(this.__getSetting("m_AmbientSkyColor", settings));
+        this.equatorColor = Color.fromRGBA(this.__getSetting("m_AmbientEquatorColor", settings));
+        this.groundColor = Color.fromRGBA(this.__getSetting("m_AmbientGroundColor", settings));
         this.intensity = this.__getSetting("m_AmbientIntensity", settings);
         this.mode = this.__getSetting("m_AmbientMode", settings);
     }
