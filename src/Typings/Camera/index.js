67678c0bdb0673049da65976c095b880d4822111
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 02:05:09 -0600

    Create index.js

---
+++ src/Typings/Camera/index.js
@@ -0,0 +1,24 @@
+const Soup = require('@stews/soup');
+const aepl = require('aepl');
+const OUT = new Soup(Object);
+
+class Camera {
+    constructor(settings) {
+        let fixedSettings = this.parent.__formSettings(this, settings);
+
+        /*
+            I tried to get normalized view port rect working but it for some reason kept failling
+            if you find a way to get it working you can reactivate it with this:
+            
+            this.normalizedViewPortRect = new this.parent.NormalizedViewPortRect(fixedSettings.normalizedViewPortRect.__value);
+
+            also if you do please @ me @paishee on discord or make an issue on the github https://github.com/paishee/unityscript/issues
+            thanks
+        */
+        this.cullingMask = new this.parent.CullingMask(fixedSettings.cullingMask.__value);
+    }
+}
+
+
+OUT.push("Camera", aepl.init("Camera", Camera));
+module.exports = OUT.pour();
