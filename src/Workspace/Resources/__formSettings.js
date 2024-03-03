0afa1996ed44b2e17df6624d5fc55377cf83d88d
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 02:03:20 -0600

    Update __formSettings.js

--- src/Workspace/Resources/__formSettings.js
+++ src/Workspace/Resources/__formSettings.js
@@ -6,10 +6,31 @@ Workspace.newF("__formSettings", function(cl, settings) {
 
     // translate something like m_OcclusionBakeSettings to occlusionBakeSettings
     fixSettings = fixSettings.mapKeys( (k, v) => {
+        
+        // removing the m_ and making the first letter lowercase
         let fixed = k.replace("m_", ""); // remove the m_
         fixed = fixed.split(""); // split it up
         if (fixed[1].toUpperCase() != fixed[1]) fixed[0] = fixed[0].toLowerCase(); // if the 2nd character isn't capital turn the first to lowercase
-        return fixed.join("");
+
+        let fixedStr = fixed.join("")
+        .replace(":", ""); // also remove : from the string
+
+        
+        if (fixedStr.includes(" ")) {
+            fixedStr = fixedStr.split(" ");
+            fixedStr = fixedStr.map( (str, i ) => {
+                if (i > 0) {
+                    str = str.split("");
+                    str[0] = str[0].toUpperCase();
+                    return str.join("");
+                }
+                else return str;
+            });
+            fixedStr = fixedStr.join("");
+        }
+
+        // return the joined string
+        return fixedStr
     });
 
     // turn everything into unityscript objects
