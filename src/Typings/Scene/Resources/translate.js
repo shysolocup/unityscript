9b836f300291dd0e2a5249c4cfa474524a661303
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 09:03:02 -0600

    Update translate.js

--- src/Typings/Scene/Resources/translate.js
+++ src/Typings/Scene/Resources/translate.js
@@ -48,17 +48,18 @@ Scene.newF("translate", function(data, meta) {
     stuff.forEach( (s, i) => {
         let name = s[0].replace(":", ""); // removes the : from the name of the property
         let { fileID } = ids[i]; // gets the file id
-        try {
-            let obj = this.__createChild(name, stuff); // creates a new child obj
-            
-            // this part is mostly just for like making sure that objects get put in Scene.objects and UnityscriptWorkspace.objects
-            if (obj instanceof this.parent.GameObject) { this.objects.push(fileID, obj); this.parent.objects.push(fileID, obj); }
-            if (obj instanceof this.parent.Light) { this.lights.push(fileID, obj); }
-            if (obj instanceof this.parent.Camera) { this.cameras.push(fileID, obj); }
+        if (this.parent[name]) {
+            try {
+                let obj = this.__createChild(name, stuff); // creates a new child obj
+                
+                // this part is mostly just for like making sure that objects get put in Scene.objects and UnityscriptWorkspace.objects
+                if (obj instanceof this.parent.GameObject) { this.objects.push(fileID, obj); this.parent.objects.push(fileID, obj); }
+                if (obj instanceof this.parent.Light) { this.lights.push(fileID, obj); }
+                if (obj instanceof this.parent.Camera) { this.cameras.push(fileID, obj); }
 
-            // finally adds it to the children
-            this.children.push(fileID, obj);
-        } catch(e) {}
-
-    })
+                // finally adds it to the children
+                this.children.push(fileID, obj);
+            } catch(e) {}
+        }
+    });
 })
