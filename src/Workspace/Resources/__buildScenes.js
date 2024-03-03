89e186365a73ee5eaf4c055be2dc254f30b4b0ee
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 10:09:39 -0600

    Update __buildScenes.js

--- src/Workspace/Resources/__buildScenes.js
+++ src/Workspace/Resources/__buildScenes.js
@@ -10,16 +10,22 @@ Workspace.newF("__buildScenes", function() { // function for building the worksp
 
 
     sceneFiles.forEach( file => {
+
+        // gets the file's name and creates a new Scene type
         let name = file.replace(".unity", ""); // name of the scene
-        const scene = new this.Scene(name); // creates a new scene typing
+        const scene = new this.Scene(name); // creates a new Scene type
 
         // sets the directory properties
         scene.dataDir = this.scenesDir+"/"+file
         scene.metaDir = scene.dataDir+".meta";
 
+        
+        // read the files
         let data = fs.readFileSync(`${this.scenesDir}/${file}`, 'utf8'); // scene data
         let meta = fs.readFileSync(`${this.scenesDir}/${file}.meta`, 'utf8'); // scene metadata
 
+
+        // translate and push
         scene.translate(data, meta); // translates the scene's metadata and data into usable JSON data
         this.scenes.push(name, scene); // pushes the new scene into the scene group
     });
