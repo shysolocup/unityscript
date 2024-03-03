b129f0f63753e72c16a41b4a10638d51ae8f741e
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 02:45:50 -0600

    Update __buildScripts.js

--- src/Workspace/Resources/__buildScripts.js
+++ src/Workspace/Resources/__buildScripts.js
@@ -13,8 +13,8 @@ Workspace.newF("__buildScripts", function() { // function for building the works
         let name = file.replace(".unity", ""); // name of the script
         const script = new this.Script(name); // creates a new script typing
 
-        let data = fs.readFileSync(`${this.scriptDir}/${file}`, 'utf8'); // script data
-        let meta = fs.readFileSync(`${this.scriptDir}/${file}.meta`, 'utf8'); // script metadata
+        let data = fs.readFileSync(`${this.scriptsDir}/${file}`, 'utf8'); // script data
+        let meta = fs.readFileSync(`${this.scriptsDir}/${file}.meta`, 'utf8'); // script metadata
 
         script.translate(data, meta); // translates the scripts's metadata and data into usable JSON data
         this.scripts.push(name, script); // pushes the new script into the script group
