d532a2543a65518d0144a3918ab305aba4d16c80
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 21:37:21 -0600

    Update index.js

--- src/Typings/BuildSettings/index.js
+++ src/Typings/BuildSettings/index.js
@@ -5,24 +5,8 @@ const util = require('util');
 
 
 class BuildSettings {
-    constructor(settings, baseSettings) {
-        Object.assign(this, baseSettings);
-        util.inherits(this.constructor, baseSettings.constructor);
-        
-        this.agentTypeID = this.__getSetting("agentTypeID", settings);
-        this.agentRadius = this.__getSetting("agentRadius", settings);
-        this.agentHeight = this.__getSetting("agentHeight", settings);
-        this.agentSlope = this.__getSetting("agentSlope", settings);
-        this.agentClimb = this.__getSetting("agentClimb", settings);
-        this.ledgeDropHeight = this.__getSetting("ledgeDropHeight", settings);
-        this.maxJumpAcrossDistance = this.__getSetting("maxJumpAcrossDistance", settings);
-        this.minRegionArea = this.__getSetting("minRegionArea", settings);
-        this.manualCellSize = this.__getSetting("manualCellSize", settings);
-        this.cellSize = this.__getSetting("cellSize", settings);
-        this.manualTileSize = this.__getSetting("manualTileSize", settings);
-        this.tileSize = this.__getSetting("tileSize", settings);
-        this.accuratePlacement = this.__getSetting("accuratePlacement", settings);
-        this.debugFlags = this.__getSetting("m_Flags", settings);
+    constructor(settings) {
+        this.parent.__formSettings(this, settings);
     }
 }
 
