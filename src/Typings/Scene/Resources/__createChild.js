7e43af48976d54dfcdeb0f824332ba282842ed1a
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 09:19:22 -0600

    Update __createChild.js

--- src/Typings/Scene/Resources/__createChild.js
+++ src/Typings/Scene/Resources/__createChild.js
@@ -3,7 +3,16 @@ const yaml = require('js-yaml');
 
 
 Scene.newF("__createChild", function(name, settings) {
+
+    // removing whitespace so it doesn't die
+    settings = settings.map( setting => setting.filter( miniSetting => {
+        return miniSetting.replace(/(\r\n|\n|\r)/gm, "").replace(/\s/, "").length > 1
+    }));
+
+    // actually loading the stuff
     let thing = settings.filter( s => s[0].includes(name))[0];
     let stuff = yaml.load(thing.join("\n"));
+
+    // returning the stuff typed
     return new this.parent[name](stuff[name]);
 });
