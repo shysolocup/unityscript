99d7a7774d0e1b41d79fb4162a72906d42885bd3
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 23:08:44 -0600

    Update index.js

--- src/Typings/Scene/index.js
+++ src/Typings/Scene/index.js
@@ -6,7 +6,9 @@ const yaml = require('js-yaml');
 
 class Scene {
     constructor() {
-
+        this.children = new this.parent.SceneChildGroup;
+        this.objects = new this.parent.SceneGameObjectGroup;
+        this.lights = new this.parent.SceneLightGroup;
     }
 
     translate(data, meta) {
@@ -15,26 +17,47 @@ class Scene {
         let stuff = data.split("--- !u");
         stuff.shift();
 
+        let ids = [];
+
         stuff = stuff.map( s => {
             s = s.split(/(\r\n|\n|\r)/gm);
-            s.shift();
+            let id = s.shift();
+            let eid = parseInt(id.split(" ")[0].replace("!", ""));
+            let fileID = parseInt(id.split(" ")[1].replace("&", ""));
+            ids.push({ id: id, eid: eid, fileID: fileID});
             s = s.filter( thing => !(thing == "\r\n") );
             return s;
         });
 
 
-        this.occlusionCullingSettings = this.createSettingChild('OcclusionCullingSettings', stuff);
-        this.renderSettings = this.createSettingChild('RenderSettings', stuff);
-        this.lightmapSettings = this.createSettingChild('LightmapSettings', stuff);
-        this.NavMeshSettings = this.createSettingChild('NavMeshSettings', stuff);
+        this.occlusionCullingSettings = this.__createChild('OcclusionCullingSettings', stuff);
+        this.renderSettings = this.__createChild('RenderSettings', stuff);
+        this.lightmapSettings = this.__createChild('LightmapSettings', stuff);
+        this.navMeshSettings = this.__createChild('NavMeshSettings', stuff);
+
 
         stuff.shift(); stuff.shift(); stuff.shift(); stuff.shift();
+        ids.shift(); ids.shift(); ids.shift(); ids.shift();
+        
+        stuff.forEach( (s, i) => {
+            let name = s[0].replace(":", "");
+            let { eid } = ids[i];
+            try {
+                let obj = this.__createChild(name, stuff);
+                
+                if (obj instanceof this.parent.GameObject) { this.objects.push(eid, obj); }
+                if (obj instanceof this.parent.Light) { this.lights.push(eid, obj); }
+
+                this.children.push(eid, obj);
+            } catch(e) {}
+
+        })
     }
 
-    createSettingChild(setting, settings) {
-        let thing = settings.filter( s => s[0].includes(setting))[0];
+    __createChild(name, settings) {
+        let thing = settings.filter( s => s[0].includes(name))[0];
         let stuff = yaml.load(thing.join("\n"));
-        return new this.parent[setting](stuff[setting]);
+        return new this.parent[name](stuff[name]);
     }
 }
 
