dc78586ab82d2193898cdbf7fd356eb35a1000b6
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 21:16:53 -0600

    Update index.js

--- src/Typings/OcclusionCullingSettings/index.js
+++ src/Typings/OcclusionCullingSettings/index.js
@@ -4,11 +4,9 @@ const OUT = new Soup(Object);
 
 class OcclusionCullingSettings {
     constructor(settings) {
-        let baseSettings = this.extend(this.parent.BaseSettings, settings);
-
-        this.occlusionBakeSettings = new this.parent.OcclusionBakeSettings(settings, baseSettings);
-        this.sceneGUID = this.__getSetting("m_SceneGUID", settings, { keepAsString: true });
-        this.occlusionCullingData = this.__getSetting("m_OcclusionCullingData", settings);
+        let fixedSettings = this.parent.__formSettings(this, settings);
+        this.extend(this.parent.BaseSettings, fixedSettings);
+        this.occlusionBakeSettings = new this.parent.OcclusionBakeSettings(fixedSettings.OcclusionBakeSettings);
     }
 }
 
