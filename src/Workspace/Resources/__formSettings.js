0efb95cffc0d8182a77ba3a024cfbb465366d6ad
paige <88659700+paishee@users.noreply.github.com>
2024-03-02 21:46:50 -0600

    Update __formSettings.js

--- src/Workspace/Resources/__formSettings.js
+++ src/Workspace/Resources/__formSettings.js
@@ -4,17 +4,20 @@ const { Workspace } = require('../index.js');
 Workspace.newF("__formSettings", function(cl, settings) {
     let fixSettings = Soup.from(settings);
 
-        fixSettings = fixSettings.mapKeys( (k, v) => {
-            let fixed = k.replace("m_", "");
-            fixed = fixed.split("");
-            if (fixed[1].toUpperCase() != fixed[1]) fixed[0] = fixed[0].toLowerCase();
-            return fixed.join("");
-        });
+    // translate something like m_OcclusionBakeSettings to occlusionBakeSettings
+    fixSettings = fixSettings.mapKeys( (k, v) => {
+        let fixed = k.replace("m_", ""); // remove the m_
+        fixed = fixed.split(""); // split it up
+        if (fixed[1].toUpperCase() != fixed[1]) fixed[0] = fixed[0].toLowerCase(); // if the 2nd character isn't capital turn the first to lowercase
+        return fixed.join("");
+    });
 
-        fixSettings.map( (k, v, i) => {
-            return new cl.parent.UnityscriptObject(k, v);
-        });
+    // turn everything into unityscript objects
+    fixSettings.map( (k, v, i) => {
+        return new cl.parent.UnityscriptObject(k, v);
+    });
 
-        Object.assign(cl, fixSettings.pour());
-        return fixSettings;
+    // assign them to the class
+    Object.assign(cl, fixSettings.pour());
+    return fixSettings;
 });
