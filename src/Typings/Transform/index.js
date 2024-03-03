f0bdfe028dc4b0c5a78d76327a2b9a9069f13b2b
paige <88659700+paishee@users.noreply.github.com>
2024-03-03 01:08:20 -0600

    Create index.js

---
+++ src/Typings/Transform/index.js
@@ -0,0 +1,21 @@
+const Soup = require('@stews/soup');
+const aepl = require('aepl');
+const OUT = new Soup(Object);
+
+class Transform {
+    constructor(settings) {
+        let fixedSettings = this.parent.__formSettings(this, settings);
+        let fileID = this.gameObject.fileID;
+
+        let obj = this.parent.objects[fileID];
+
+        Object.assign(obj, this);
+        delete obj.gameObject;
+
+        obj.transform = this;
+    }
+}
+
+
+OUT.push("Transform", aepl.init("Transform", Transform));
+module.exports = OUT.pour();
