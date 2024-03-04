77fe57f48f4ee164a1ab3bcfc123f88db3eb19be
paige <88659700+paishee@users.noreply.github.com>
2024-03-04 06:24:12 -0600

    Update __generateID.js

--- src/Workspace/Resources/__generateID.js
+++ src/Workspace/Resources/__generateID.js
@@ -3,7 +3,8 @@ const { random } = require('stews');
 
 
 Workspace.newF("__generateID", function(length=5, characters=null) {
-    characters = (characters) ? characters : "abcdefghijklmnopqrstuvwxyz1234567890-_".split("");
+    characters = (characters) ? characters : "abcdefghijklmnopqrstuvwxyz1234567890-_"
+    if (typeof characters == "string") characters = characters.split("");
     let id = "";
     
     for (let i = 0; i < length; i++) {
