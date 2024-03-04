5ce4e7f5423325751bb41418e8a1f25f2ba771d8
paige <88659700+paishee@users.noreply.github.com>
2024-03-04 14:12:06 -0600

    Update index.js

--- src/Typings/BaseType/index.js
+++ src/Typings/BaseType/index.js
@@ -5,19 +5,25 @@ const OUT = new Soup(Object);
 
 class BaseType {
     constructor(settings) {
-        this.parent.__formSettings(this, settings);
-
         try {
             if (settings.__value) settings = settings.__value;
         } catch(e) { }
 
+        this.parent.__formSettings(this, settings);
+
         if (this.objectHideFlags) {
+            /*
             let { ObjectHideFlags } = require('../../Workspace');
+
+            try {
+                if (this.objectHideFlags.__value) this.objectHideFlags = this.objectHideFlags.__value;
+            } catch(e) { }
 
             if (this.objectHideFlags == false) this.objectHideFlags = 0;
             else if (this.objectHideFlags == true) this.objectHideFlags = 1;
 
-            this.objectHideFlags = Object.keys(ObjectHideFlags)[this.objectHideFlags];
+            this.objectHideFlags = new this.parent.UnityscriptObject("objectHideFlags", Object.keys(ObjectHideFlags)[this.objectHideFlags]);
+            */
         }
     }
 }
