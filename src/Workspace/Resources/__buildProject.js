be26a314b8bd01fc12721381ff5c9c2f8944823a
paige <88659700+paishee@users.noreply.github.com>
2024-03-04 13:10:03 -0600

    Update __buildProject.js

--- src/Workspace/Resources/__buildProject.js
+++ src/Workspace/Resources/__buildProject.js
@@ -6,14 +6,13 @@ const fs = require('fs');
 Workspace.newF("__buildProject", function() { // function for building the project's settings
     
     // a list of project settings
-    let settingFiles = fs.readdirSync(this.settingsDir).filter( file => !file.endsWith(".asset"));
+    let settingFiles = fs.readdirSync(this.settingsDir).filter( file => file.endsWith(".asset"));
 
     
     settingFiles.forEach( file => {
 
         // gets the file's name and creates a new Scene type
         let name = file.replace(".asset", ""); // name of the setting group
-        const setting = new this.ProjectSetting(name);
 
         
         // read the files
@@ -21,7 +20,6 @@ Workspace.newF("__buildProject", function() { // function for building the proje
 
 
         // translate and push
-        setting.translate(data); // translates the setting's metadata and data into usable JSON data
-        this.settings.push(name, setting); // pushes the new project setting 
+        this.settings.translate(data); // translates the setting's metadata and data into usable JSON data
     });
 });
