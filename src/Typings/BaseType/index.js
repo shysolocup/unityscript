5cbdc8e24f65b6f5ecef4a78e0d8e9f2e6baa8ed
paige <88659700+paishee@users.noreply.github.com>
2024-03-04 13:38:40 -0600

    Update index.js

--- src/Typings/BaseType/index.js
+++ src/Typings/BaseType/index.js
@@ -6,6 +6,15 @@ const OUT = new Soup(Object);
 class BaseType {
     constructor(settings) {
         this.parent.__formSettings(this, settings);
+
+        if (this.objectHideFlags) {
+            let { ObjectHideFlags } = require('../../Workspace');
+
+            if (this.objectHideFlags == false) this.objectHideFlags = 0;
+            else if (this.objectHideFlags == true) this.objectHideFlags = 1;
+
+            this.objectHideFlags = Object.keys(ObjectHideFlags)[this.objectHideFlags];
+        }
     }
 }
 
