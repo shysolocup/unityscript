16046b693cadff9787dde81a07e1fc139206e575
paige <88659700+paishee@users.noreply.github.com>
2024-03-04 06:19:38 -0600

    Create __generateID.js

---
+++ src/Workspace/Resources/__generateID.js
@@ -0,0 +1,14 @@
+const Workspace = require('../index.js');
+const random = require('@stews/random');
+
+
+Workspace.newF("__generateID", function(length=5, characters=null) {
+    characters = (characters) ? characters : "abcdefghijklmnopqrstuvwxyz1234567890-_".split("");
+    let id = "";
+    
+    for (let i = 0; i < length; i++) {
+        id += random.choice(characters);
+    }
+
+    return id;
+});
