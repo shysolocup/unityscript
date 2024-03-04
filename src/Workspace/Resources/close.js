7369afa640e038a98168ce000c7965e5d036e596
paige <88659700+paishee@users.noreply.github.com>
2024-03-04 12:38:31 -0600

    Update close.js

--- src/Workspace/Resources/close.js
+++ src/Workspace/Resources/close.js
@@ -3,5 +3,5 @@ const { Workspace } = require('../index.js');
 
 Workspace.newF("close", function() { // function for closing the workspace
     clearInterval(this.aliveInterval); // kills the process
-    this.events.close.fire(this, this.aliveInterval); // fires the close event
+    this.events.close.fire(); // fires the close event
 });
