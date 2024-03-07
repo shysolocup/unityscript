45dc2e3983070a91cfa6b883b11b7e54cf684a90
paige <88659700+paishee@users.noreply.github.com>
2024-03-07 12:41:43 -0600

    Create Start.cs

---
+++ src/CS/ScriptParts/Main/Start.cs
@@ -0,0 +1,13 @@
+/* 
+  Compiled by UnityScript
+  https://github.com/paishee/unityscript
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+    
+    
+void Start() {
