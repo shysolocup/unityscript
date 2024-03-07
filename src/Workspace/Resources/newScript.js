4e1f5c3ba64dfb2a00fd467140f06e270db8ffc1
paige <88659700+paishee@users.noreply.github.com>
2024-03-07 13:07:41 -0600

    Create newScript.js

---
+++ src/Workspace/Resources/newScript.js
@@ -0,0 +1,22 @@
+const { Workspace } = require('../index.js');
+const fs = require('fs');
+
+
+Workspace.newF("newScript", function(name, data) { // function for opening the workspace
+
+    function getPart(fileName) {
+        return fs.readFileSync(`${this.mainPartsDir}/${fileName}.cs`, 'utf8')
+    }
+
+    var fileData = [];
+    
+    const parts = {
+        start: getPart("start"),
+        update: getPart("update"),
+        end: getPart("end")
+    };
+
+    fileData.push(parts.start.replace("SCRIPT_NAME_HERE", name));
+
+    fs.writeFileSync(`${this.scriptsDir}/${name}.cs` fileData.join("\n"));
+});
